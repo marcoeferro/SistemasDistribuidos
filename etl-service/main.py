9ee730583c4c957880aee73536003a10241bdadd
Marco Ferro <marcoeferro2@gmail.com>
2024-10-04 02:23:55 -0300

    fix(etl-service): update imports for extraction methods after folder restructure

--- etl-service/main.py
+++ etl-service/main.py
@@ -1,13 +1,13 @@
 import requests
 import json
-from login import login
-from data_batery_info_chart import get_data_batery_info_chart
-from data_discharging import get_data_discharging
-from data_energy_area_chart import get_data_energy_area_chart
-from data_energy_bar_chart import get_data_energy_chart
-from data_photovoltaic_device import get_data_photovoltaic_device
-from data_wheater import get_data_wheater
-from data_plant import get_data_plant
+from src.scripts.extract.login import login
+from src.scripts.extract.data_batery_info_chart import get_data_batery_info_chart
+from src.scripts.extract.data_discharging import get_data_discharging
+from src.scripts.extract.data_energy_area_chart import get_data_energy_area_chart
+from src.scripts.extract.data_energy_bar_chart import get_data_energy_chart
+from src.scripts.extract.data_photovoltaic_device import get_data_photovoltaic_device
+from src.scripts.extract.data_wheater import get_data_wheater
+from src.scripts.extract.data_plant import get_data_plant
 
 loginUrl = "https://server.growatt.com/login"
 
@@ -25,23 +25,23 @@ plant = get_data_plant(session=session, loginUrl=loginUrl)
 
 # Writing to .json files
 
-with open("./data/batery_info.json", "w") as outfile:
+with open("./data/raw/batery_info.json", "w") as outfile:
     outfile.write(json.dumps(batery_info))
 
-with open("./data/discharging.json", "w") as outfile:
+with open("./data/raw/discharging.json", "w") as outfile:
     outfile.write(json.dumps(discharging))
 
-with open("./data/energy_area_chart.json", "w") as outfile:
+with open("./data/raw/energy_area_chart.json", "w") as outfile:
     outfile.write(json.dumps(energy_area_chart))
 
-with open("./data/energy_bar_chart.json", "w") as outfile:
+with open("./data/raw/energy_bar_chart.json", "w") as outfile:
     outfile.write(json.dumps(energy_bar_chart))
 
-with open("./data/photovoltaic_device.json", "w") as outfile:
+with open("./data/raw/photovoltaic_device.json", "w") as outfile:
     outfile.write(json.dumps(photovoltaic_device))
 
-with open("./data/wheater.json", "w") as outfile:
+with open("./data/raw/wheater.json", "w") as outfile:
     outfile.write(json.dumps(wheater))
 
-with open("./data/plant.json", "w") as outfile:
+with open("./data/raw/plant.json", "w") as outfile:
     outfile.write(json.dumps(plant))
