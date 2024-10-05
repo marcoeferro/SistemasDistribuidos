890dabe9de104920e524b5384177401d5939e754
Marco Ferro <marcoeferro2@gmail.com>
2024-10-05 00:51:48 -0300

    feat(etl-service): add transformation script to obtain only the required battery information data

---
+++ etl-service/src/scripts/transform/transform_batery_info.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from utils.json_utils import get_values_by_keys, json_traverser
+import json
+
+# Define the base directory (e.g., the directory of the current script)
+base_dir = Path(__file__).resolve().parent
+
+# Define the raw data directory relative to the base directory
+raw_data_dir = base_dir/'..'/'..'/'..'/'data'/'raw'
+
+# Define the path to the raw batery_info JSON file
+raw_file_path = raw_data_dir / 'batery_info.json'
+
+
+# Define the raw data directory relative to the base directory
+processed_data_dir = base_dir/'..'/'..'/'..'/'data'/'processed'
+
+# Define the path to the processed batery_info JSON file
+processed_file_path = processed_data_dir / 'processed_batery_info.json'
+
+
+
+with raw_file_path.open("r") as file:
+    data = json.load(file)
+    
+    data_raw = json_traverser(data)
+    data_cleaned = get_values_by_keys(data=data_raw,keys=['date','cdsTitle','capacity','cd_charge','cd_disCharge'])
+
+    with processed_file_path.open('w') as outfile:
+        json.dump(obj=data_cleaned,fp=outfile)
+
+
+
