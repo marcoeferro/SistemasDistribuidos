732e9d27b684b4b5bd3720fe19afb3d08978f81f
Marco Ferro <marcoeferro2@gmail.com>
2024-10-05 00:33:36 -0300

    fix(etl-service): add headers to avoid 403 response

--- etl-service/src/scripts/extract/login.py
+++ etl-service/src/scripts/extract/login.py
@@ -15,10 +15,25 @@ def login(loginUrl:str,session:requests) -> requests:
         "validateCode":""
     }
 
+    headers = {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5,es-MX;q=0.4",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua": "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "referer": loginUrl,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
     response = session.post(
         loginUrl,
         data=payload,
-        headers=dict(referer=loginUrl)
+        headers=headers
     )
 
     if (response.status_code == 200 ):
