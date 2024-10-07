99f389f769e577a5f369b5c6dfde5516f910a319
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:27:44 -0300

    refactor(script): Rename file creator, update imports, fix type issues

--- etl-service/src/scripts/transform/transform_batery_info.py
+++ etl-service/src/scripts/transform/transform_batery_info.py
@@ -1,33 +1,27 @@
 from pathlib import Path
-import path_setup # This ensures the sys.path is modified
+from . import path_setup # This ensures the sys.path is modified
 from utils.json_utils import get_values_by_keys, json_traverser
 import json
-
-def transform_batery_info() -> None:
+import datetime
+def transform_batery_info(file) -> json:
 
     # Define the base directory (e.g., the directory of the current script)
     base_dir = Path(__file__).resolve().parent
-
-    # Define the raw data directory relative to the base directory
-    raw_data_dir = base_dir/'..'/'..'/'..'/'data'/'raw'
-
-    # Define the path to the raw batery_info JSON file
-    raw_file_path = raw_data_dir / 'batery_info.json'
 
 
     # Define the raw data directory relative to the base directory
     processed_data_dir = base_dir/'..'/'..'/'..'/'data'/'processed'
 
     # Define the path to the processed batery_info JSON file
-    processed_file_path = processed_data_dir / 'processed_batery_info.json'
+    processed_file_path = processed_data_dir / f'processed_batery_info_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.json'
 
     labels = ['date','cdsTitle','capacity','cd_charge','cd_disCharge']
+    
+    data = file
+    
+    data_raw = json_traverser(data)
+    data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
 
-    with raw_file_path.open("r") as file:
-        data = json.load(file)
-        
-        data_raw = json_traverser(data)
-        data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
-
-        with processed_file_path.open('w') as outfile:
-            json.dump(obj=data_cleaned,fp=outfile)
+    with processed_file_path.open('w') as outfile:
+        json.dump(obj=data_cleaned,fp=outfile)
+        return data_cleaned
