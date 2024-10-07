6efe6a539843cb7f8c9cd4904c4e5a813cab43b1
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:28:02 -0300

    refactor(script): Rename file creator, update imports, fix type issues

--- etl-service/src/scripts/transform/transform_photovoltaic_device.py
+++ etl-service/src/scripts/transform/transform_photovoltaic_device.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-import path_setup # This ensures the sys.path is modified
+from . import path_setup # This ensures the sys.path is modified
 from utils.json_utils import get_values_by_keys, json_traverser
 import json
+import datetime
 
-def transform_photovoltaic_device() -> None:
+def transform_photovoltaic_device(file) -> json:
 
     # Define the base directory (e.g., the directory of the current script)
     base_dir = Path(__file__).resolve().parent
@@ -19,19 +20,21 @@ def transform_photovoltaic_device() -> None:
     processed_data_dir = base_dir/'..'/'..'/'..'/'data'/'processed'
 
     # Define the path to the processed photovoltaic_device JSON file
-    processed_file_path = processed_data_dir / 'processed_photovoltaic_device.json'
+    processed_file_path = processed_data_dir / f'processed_photovoltaic_device_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.json'
 
 
-    labels = ["deviceType","ptoStatus","timeServer","accountName","timezone","plantId","deviceTypeName","nominalPower","bdcStatus","eToday","eMonth","datalogTypeTest","eTotal","pac","datalogSn","alias","location","deviceModel","sn","plantName","status","lastUpdateTime"]
-    with raw_file_path.open("r") as file:
-        data = json.load(file)
-        
-        data_raw = json_traverser(data)
-        
-        data_raw.update(json_traverser(dict(data_raw["datas"][0])))
-        
-        print(f"LEAFS NODES {data_raw}")
-        data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
+    labels = ["accountName", "alias", "bdcStatus", "datalogSn", "datalogTypeTest", "deviceModel", "deviceType", "deviceTypeName", "eMonth", "eToday", "eTotal", "lastUpdateTime", "location", "nominalPower", "pac", "plantId", "plantName", "ptoStatus", "sn", "status", "timeServer", "timezone"]
+    
+    
+    data = file
+    
+    data_raw = json_traverser(data)
+    
+    data_raw.update(json_traverser(dict(data_raw["datas"][0])))
+    data_raw.pop("datas")
+    
+    data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
 
-        with processed_file_path.open('w') as outfile:
-            json.dump(obj=data_cleaned,fp=outfile)
+    with processed_file_path.open('w') as outfile:
+        json.dump(obj=data_cleaned,fp=outfile)
+        return data_cleaned
