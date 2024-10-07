951f8c88bc497b63b8449bcbe6c90a5d717442b2
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:28:14 -0300

    refactor(script): Rename file creator, update imports, fix type issues

--- etl-service/src/scripts/transform/transform_wheater.py
+++ etl-service/src/scripts/transform/transform_wheater.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-import path_setup # This ensures the sys.path is modified
+from . import path_setup # This ensures the sys.path is modified
 from utils.json_utils import get_values_by_keys, json_traverser
 import json
+import datetime
 
-def transform_wheater() -> None:
+def transform_wheater(file) -> json:
 
     # Define the base directory (e.g., the directory of the current script)
     base_dir = Path(__file__).resolve().parent
@@ -19,16 +20,17 @@ def transform_wheater() -> None:
     processed_data_dir = base_dir/'..'/'..'/'..'/'data'/'processed'
 
     # Define the path to the processed wheater JSON file
-    processed_file_path = processed_data_dir / 'processed_wheater.json'
+    processed_file_path = processed_data_dir / f'processed_wheater_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.json'
 
-    labels = ["city","Week","dataStr","cloud","hum","wind_deg","pres","pcpn","fl","tmp","wind_sc","cond_txt","wind_dir","wind_spd","cond_code","loc","utc","ss","admin_area","toDay","location","parent_city","cnty","sr","status","radiant","tempType"]
+    labels = ["Week","city","admin_area","cnty","location","parent_city","sr","ss","toDay","cloud","cond_code","cond_txt","fl","hum","pcpn","pres","tmp","wind_deg","wind_dir","wind_sc","wind_spd","status","loc","utc","dataStr","radiant","tempType"]
 
-    with raw_file_path.open("r") as file:
-        data = json.load(file)
-        
-        data_raw = json_traverser(data)
-        data_raw.update(json_traverser(dict(data_raw["HeWeather6"][0])))
-        data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
+    
+    data = file
+    
+    data_raw = json_traverser(data)
+    data_raw.update(json_traverser(dict(data_raw["HeWeather6"][0])))
+    data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
 
-        with processed_file_path.open('w') as outfile:
-            json.dump(obj=data_cleaned,fp=outfile)
+    with processed_file_path.open('w') as outfile:
+        json.dump(obj=data_cleaned,fp=outfile)
+        return data_cleaned
