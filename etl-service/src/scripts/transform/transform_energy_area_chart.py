158c0651289175cfbe2125ec51b860a94f614705
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:27:53 -0300

    refactor(script): Rename file creator, update imports, fix type issues

--- etl-service/src/scripts/transform/transform_energy_area_chart.py
+++ etl-service/src/scripts/transform/transform_energy_area_chart.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import json
-import path_setup # This ensures the sys.path is modified
+from . import path_setup # This ensures the sys.path is modified
 from utils.json_utils import get_values_by_keys, json_traverser
-
-def transform_energy_area_chart() -> None:
+import datetime
+def transform_energy_area_chart(file) -> json:
     # Define the base directory (e.g., the directory of the current script)
     base_dir = Path(__file__).resolve().parent
 
@@ -18,15 +18,16 @@ def transform_energy_area_chart() -> None:
     processed_data_dir = base_dir/'..'/'..'/'..'/'data'/'processed'
 
     # Define the path to the processed energy_area_chart JSON file
-    processed_file_path = processed_data_dir / 'processed_energy_area_chart.json'
+    processed_file_path = processed_data_dir / f'processed_energy_area_chart_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.json'
 
-    labels = ["vPv2","deviceType","gridPower","loadPower","vPv1","fAcOutput","invStatus","ppv2","vBat","loadPrecent","panelPower","batPower","vAcOutput","capacity","ppv1","iPv1","iPv2","vAcInput","fAcInput","iTotal","rateVA","status"]
+    labels = ["eChargeTotal","pacToGrid","ppv","sysOut","userLoad","pacToUser","eAcDisCharge","eDisCharge","eCharge","eAcCharge","eDisChargeTotal"]
 
-    with raw_file_path.open("r") as file:
-        data = json.load(file)
-        
-        data_raw = json_traverser(data)
-        data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
+    
+    data = file
+    
+    data_raw = json_traverser(data)
+    data_cleaned = get_values_by_keys(data=data_raw,keys=labels)
 
-        with processed_file_path.open('w') as outfile:
-            json.dump(obj=data_cleaned,fp=outfile)
+    with processed_file_path.open('w') as outfile:
+        json.dump(obj=data_cleaned,fp=outfile)
+        return data_cleaned
