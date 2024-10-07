3f0b5580fff5d7531a5d72c1bfa59f5001912e05
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:21:04 -0300

    fix(scraper): Change the name of the function and signage to improve redability

--- etl-service/src/scripts/extract/extract_plant_data.py
+++ etl-service/src/scripts/extract/extract_plant_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def get_data_plant(session: requests, loginUrl: str) -> json:
+def get_plant_data(session: requests, loginUrl: str) -> json:
     """
     Args: 
         requests: the login session 
