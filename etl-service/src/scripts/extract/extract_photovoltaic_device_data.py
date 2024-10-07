eb73ebe4760544697f15e6ab21f1fe280c7d859b
Marco Ferro <marcoeferro2@gmail.com>
2024-10-06 23:20:53 -0300

    fix(scraper): Change the name of the function and signage to improve redability

--- etl-service/src/scripts/extract/extract_photovoltaic_device_data.py
+++ etl-service/src/scripts/extract/extract_photovoltaic_device_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def get_data_photovoltaic_device(session: requests, loginUrl: str) -> json:
+def get_photovoltaic_device_data(session: requests, loginUrl: str) -> json:
     """
     Args: 
         requests: the login session 
